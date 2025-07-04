da10162f87dc51b4bd383576f4fd5223a3f4dd09
mamduh5 <mamduh2542@gmail.com>
2025-07-04 21:51:29 +0700

    remove next js

--- frontend/src/services/test.js
+++
@@ -1,9 +0,0 @@
-//for specific middleware / api
-
-// src/services/userService.js
-import api from '@/lib/api';
-
-export async function getUsers() {
-  const res = await api.get('/users');
-  return res.data;
-}
