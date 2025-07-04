fa394ad3bf0d656652bd16b942383dae2a35236e
mamduh5 <mamduh2542@gmail.com>
2025-07-04 22:22:05 +0700

    sending up frontend using react + vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'; // Keep the styling if you want, or remove/modify
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Harmster Thief !!</h1>
+      {/* This is where your game components will go */}
+      <p>Your journey to master React begins here.</p>
+    </div>
+  );
+}
+
+export default App;
